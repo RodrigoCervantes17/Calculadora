184475cff540aea33b4692e6ac1111aa695ffdbd
RodrigoCervantes17 <99071743+RodrigoCervantes17@users.noreply.github.com>
2025-11-03 14:50:54 -0600

    Solved possible app-breaking bugs, such as non-numerical inputs or division by 0.

--- Program.cs
+++ Program.cs
@@ -16,11 +16,21 @@ class Program
             Console.WriteLine("2. Resta");
             Console.WriteLine("3. Multiplicacion");
             Console.WriteLine("4. Division");
+            Console.WriteLine("5. Salir");
             string? opcion = Console.ReadLine();
             Console.WriteLine("Ingrese el primer numero:");
-            float num1 = float.Parse(Console.ReadLine()!);
-            Console.WriteLine("Ingrese el segundo numero:");
-            float num2 = float.Parse(Console.ReadLine()!);
+            float num1, num2;
+            while (!float.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Por favor ingresa un número válido:");
+            }
+
+            Console.WriteLine("Ingrese el segundo número:");
+            while (!float.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("Por favor ingresa un número válido:");
+            }
+
 
             switch (opcion)
             {
@@ -37,11 +47,30 @@ class Program
                     Console.WriteLine($"El resultado de la multiplicacion es: {resultado}");
                     break;
                 case "4":
-                    resultado = num1 * num2;
-                    Console.WriteLine($"El resultado de la division es: {resultado}");
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                        Console.WriteLine($"El resultado de la división es: {resultado}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: no se puede dividir entre cero.");
+                    }
+                    break;
+                case "5":
+                    continuar = false;
+                    Console.WriteLine("Saliendo de la calculadora. ¡Hasta luego!");
                     break;
 
             }
+            Console.WriteLine("Continuar? s/n");// Línea en blanco para mejor legibilidad
+            string? sigOperacion = Console.ReadLine();
+            if (sigOperacion.ToLower()!= "s")
+            {
+                continuar = false;
+                Console.WriteLine("Saliendo de la calculadora. ¡Hasta luego!");
+            }
+            Console.Clear();
         }
     }
 }
